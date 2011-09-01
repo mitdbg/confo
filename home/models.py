2f315167dd1823f96a2464b4074070297ec428de
Adam Marcus <marcua@marcua.net>
2011-09-01 13:55:05 -0400

    initial models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Conference(models.Model):
+    short = models.CharField(max_length=30)
+    full = models.TextField()
+
+class ConfYear(models.Model):
+    conf = models.ForeignKey(Conference)
+    year = models.DateField()
+
+class Author(models.Model):
+    name = models.TextField()
+
+class Paper(models.Model):
+    conf = models.ForeignKey(ConfYear)
+    authors = models.ManyToManyField(Author, related_name="papers")
+    title = model.TextField()
+
+    
