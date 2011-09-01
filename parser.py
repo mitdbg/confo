ebc4268f6868637f8072d5d9a87f3524a1555ea5
Adam Marcus <marcua@marcua.net>
2011-09-01 13:42:28 -0400

    parser now reads all papers, in preparation for adding to a dbms

--- parser.py
+++ parser.py
@@ -6,37 +6,64 @@ paperinfo = None
 curkey = None
 stop = set(stopwords.words('english'))
 
-def process_paper(yearcounts, paperinfo):
-    year = int(paperinfo["year"])
-    title = paperinfo["title"]
-    
-    wordcount = yearcounts.get(year, {})
-    words = [word for word in title.lower().split() if word not in stop]
-    for word in words:
-        word = word.rstrip(",.?:;'\"()[]-_=+&!~`")
-        count = wordcount.get(word, 0)
-        wordcount[word] = count + 1
-    yearcounts[year] = wordcount
+class FormatError(Exception):
+    def __init__(self, message):
+        self.message = message
 
-def parse(filename, confid):
-    yearcounts = {}
-    confkey = "conf/" + confid + "/"
-    storekeys = set(("year", "title"))
+class UniqueIder():
+    def __init__(self):
+        self.__id = 0
+        self.__mapping = {}
+    def get_id(self, key):
+        if key in self.__mapping:
+            retval = self.__mapping[key]
+        else:
+            retval = self.__id + 1
+            self.__mapping[key] = retval
+            self.__id = retval
+        return retval
+    def items(self):
+        return dict(self.__mapping)
+
+def get_conf(crossref):
+    parts = crossref.split("/")
+    if parts != "conf" and len(parts) < 2:
+        raise FormatError("crossref '%s' was unexpected" % crossref)
+    return parts[1]
+
+def process_paper(paperinfo, author_dict, conf_dict):
+    year = int(paperinfo["year"][0])
+    title = paperinfo["title"][0]
+    conf = get_conf(paperinfo.get("crossref", [""])[0])
+    authors = paperinfo.get("author", [])
+    
+#    print year, conf, title
+    
+def parse(filename):
+    authors = UniqueIder()
+    conferences = UniqueIder()
+    storekeys = set(("year", "title", "author", "crossref"))
     def start_element(name, attrs):
         global paperinfo, curkey
-        if name == "inproceedings" and attrs["key"].startswith(confkey):
+        if name == "inproceedings":
             paperinfo = {}
         elif paperinfo != None and name in storekeys:
             curkey = name
     def end_element(name):
         global paperinfo, curkey
-        if paperinfo != None and name == "inproceedings":
-            process_paper(yearcounts, paperinfo)
-            paperinfo = None
+        try:
+            if paperinfo != None and name == "inproceedings":
+                process_paper(paperinfo, authors, conferences)
+                paperinfo = None
+        except FormatError as e:
+            sys.stderr.write("Formatting exception: %s on %s\n" % (e.message, repr(paperinfo)))
+            
     def char_data(data):
         global paperinfo, curkey
         if curkey != None:
-            paperinfo[curkey] = data
+            vals = paperinfo.get(curkey, [])
+            vals.append(data)
+            paperinfo[curkey] = vals
             curkey = None
 
     p = xml.parsers.expat.ParserCreate()
@@ -57,10 +84,7 @@ def sortcounts(yearcounts):
     return sortedcounts
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "arguments: conferencekey"
-        sys.exit(-1)
-    yearcounts = parse("dblp.xml", sys.argv[1])
+    yearcounts = parse("dblp.xml")
     sortedcounts = sortcounts(yearcounts)
     for year, sortedwords in sortedcounts:
         print year
