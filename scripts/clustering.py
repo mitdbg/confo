46185d3560edde38ac66f5f5f57368c83268181b
Adam Marcus <marcua@marcua.net>
2011-09-15 18:07:20 -0400

    refactoring clustering to make actor clustering easier

---
+++ scripts/clustering.py
@@ -0,0 +1,84 @@
+import os
+import sys
+from util import similarity
+
+ROOT = os.path.abspath('%s/../..' % os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(ROOT)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'confo.settings'
+from django.core.management import setup_environ
+from confo import settings
+setup_environ(settings)
+from confo.home.models import *
+from django.db import connection, transaction
+from django.db.models import *
+
+from operator import itemgetter
+
+class Clusterer:
+    def __init__(self, table_getter, query, topn, vector_size, unique_frac):
+        self.table_getter = table_getter
+        self.query = query
+        self.topn = topn
+        self.vector_size = vector_size
+        self.unique_frac = unique_frac
+
+    def count_and_index(self, cur):
+        print "Executing count query"
+        cur.execute(self.query)
+        print "Query executed, generating vectors and inverted index"
+        counts = {}
+        invert = {}
+        for iid, wid, count in cur:
+            item_count = counts.get(iid, [])
+            if len(item_count) == VECTOR_SIZE:
+                continue
+            item_count.append((wid, float(count)))
+            if len(item_count) == 1:
+                counts[iid] = item_count
+
+            word = invert.get(wid, set())
+            invert.add(iid)     
+            if len(word) == 1:
+                count[wid] = word
+        return (counts, invert)
+
+    def calc_similarities(self, cid, vector, candidates, counts):
+        similar = []
+        for cand in candidates:
+            if cid != cand:
+                cand_vector = counts[cand]
+                sim = similarity(vector, cand_vector)
+                sc = {'fromconf': cid, 'toconf': cand, 'similarity': sim}
+                similar.append(sc)
+        return similar
+
+    def write_topn(self, similar, table):
+        sort_sim = sorted(similar, key=itemgetter('similarity'), reverse=True)
+        keepers = sort_sim[:TOPN]
+        for keeper in keepers:
+            table.get(None, keeper, False)
+
+    def write_similar(self, counts, invert):
+        print "Calculating similarities, writing to file"
+        write_table = self.table_getter("w")
+        for cid, vector in counts.items():
+            cand_sets = [invert[wid] for wid, count in vector]
+            cand_sets = filter(lambda x: len(x) < UNIQUE_FRAC * len(counts), cand_sets)
+            if len(cand_sets) > 0:
+                candidates = reduce(lambda x, y: x | y, cand_sets)
+                similar = self.calc_similarities(cid, vector, candidates, counts)
+                self.write_topn(similar, write_table)
+        write_table.close()
+
+    def load_similar(self, cur):
+        print "Loading file into similar conferences table"
+        read_table = self.table_getter("r")
+        read_table.logtodb(cur)
+
+    @transaction.commit_on_success
+    def build_similar(self):
+        cur = connection.cursor()
+        counts, invert = self.count_and_index(cur)
+        self.write_similar(counts, invert)
+        transaction.commit()
+        self.load_similar(cur)
--- scripts/similar_conferences.py
+++ scripts/similar_conferences.py
@@ -1,95 +1,20 @@
-import os
-import sys
-from util import similarity
-
-ROOT = os.path.abspath('%s/../..' % os.path.abspath(os.path.dirname(__file__)))
-sys.path.append(ROOT)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'confo.settings'
-from django.core.management import setup_environ
-from confo import settings
-setup_environ(settings)
+from clustering import Clusterer
 from confo.home.models import *
-from django.db import connection, transaction
-from django.db.models import *
-
 from dblogging import LogOrCache
-from operator import itemgetter
 
 TOPN = 10
 VECTOR_SIZE = 20
 UNIQUE_FRAC = .05
 
-def confs_words(cur):
-    print "Executing conference count query"
+def get_table(mode):
+    return LogOrCache(["fromconf","toconf","similarity"], "similar_conferences.txt", mode, "similar_conferences")
+
+if __name__ == '__main__':
+    SimilarConferences.objects.all().delete()
     q = """select y.cid, ywc.word, sum(ywc.count) AS total
            from years as y, year_word_counts as ywc
            where y.id = ywc.yid
            group by y.cid, ywc.word
            order by y.cid, total DESC;"""
-    cur.execute(q)
-    print "Query executed, generating vectors and inverted index"
-    confs = {}
-    words = {}
-    for cid, wid, count in cur:
-        conf = confs.get(cid, [])
-        if len(conf) == VECTOR_SIZE:
-            continue
-        conf.append((wid, float(count)))
-        if len(conf) == 1:
-            confs[cid] = conf
-
-        word = words.get(wid, set())
-        word.add(cid)     
-        if len(word) == 1:
-            words[wid] = word
-    return (confs, words)
-
-
-def calc_similarities(cid, vector, candidates, conf_words):
-    similar = []
-    for cand in candidates:
-        if cid != cand:
-            cand_vector = conf_words[cand]
-            sim = similarity(vector, cand_vector)
-            sc = {'fromconf': cid, 'toconf': cand, 'similarity': sim}
-            similar.append(sc)
-    return similar
-
-def write_topn(similar, table):
-    sort_sim = sorted(similar, key=itemgetter('similarity'), reverse=True)
-    keepers = sort_sim[:TOPN]
-    for keeper in keepers:
-        table.get(None, keeper, False)
-
-def get_table(mode):
-    return LogOrCache(["fromconf","toconf","similarity"], "similar_conferences.txt", mode, "similar_conferences")
-
-def write_similar(conf_words, word_confs):
-    print "Calculating similarities, writing to file"
-    write_table = get_table("w")
-    for cid, vector in conf_words.items():
-        cand_sets = [word_confs[wid] for wid, count in vector]
-        cand_sets = filter(lambda x: len(x) < UNIQUE_FRAC * len(conf_words), cand_sets)
-        if len(cand_sets) > 0:
-            candidates = reduce(lambda x, y: x | y, cand_sets)
-            similar = calc_similarities(cid, vector, candidates, conf_words)
-            write_topn(similar, write_table)
-    write_table.close()
-
-def load_similar(cur):
-    print "Loading file into similar conferences table"
-    read_table = get_table("r")
-    read_table.logtodb(cur)
-
-@transaction.commit_on_success
-def similar_conferences():
-    SimilarConferences.objects.all().delete()
-    cur = connection.cursor()
-    conf_words, word_confs = confs_words(cur)
-    write_similar(conf_words, word_confs)
-    transaction.commit()
-    load_similar(cur)
-
-if __name__ == '__main__':
-    similar_conferences()
-
+    c = Clusterer(get_table, q, TOPN, VECTOR_SIZE, UNIQUE_FRAC)
+    c.build_similar()
